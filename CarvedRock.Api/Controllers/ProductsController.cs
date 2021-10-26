44aba23b4df7b0ed682f7207bf477eb7df97a02a
jessica.brisson <jessica.brisson@gov.ab.ca>
2021-10-26 10:32:03 -0600

    init project commit

---
+++ CarvedRock.Api/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using CarvedRock.Api.ApiModels;
+using CarvedRock.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CarvedRock.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+        private readonly IProductLogic _productLogic;
+
+        public ProductsController(ILogger<ProductsController> logger, IProductLogic productLogic)
+        {
+            _logger = logger;
+            _productLogic = productLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts(string category = "all")
+        {
+            _logger.LogInformation(message: "Test this logging message from getproducts controller for {category}", category);
+            return _productLogic.GetProductsForCategory(category);
+        }
+    }
+}
